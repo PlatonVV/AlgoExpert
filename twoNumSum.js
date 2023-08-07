c40823cd4f3546183e14b4ed138e04049ef5a4a6
PlatonVV <platonvolodin@icloud.com>
2023-08-07 11:01:42 -0400

    Initial commit

---
+++ twoNumSum.js
@@ -0,0 +1,44 @@
+// O(n^2) time | O(1) space
+function twoNumberSum(array, targetSum) {
+  for (let i = 0; i < array.length - 1; i++) {
+    const firstNum = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      const secondNum = array[j];
+      if (firstNum + secondNum === targetSum) {
+        return [firstNum, secondNum];
+      }
+    }
+  }
+  return [];
+}
+
+// 0(n) time | 0(1) space
+function twoNumberSum(array, targetSum) {
+  const nums = {};
+  for (const num of array) {
+    let possibleMatch = targetSum - num;
+    if (possibleMatch in nums) {
+      return [possibleMatch, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
+}
+
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
---
+++ validate-subsequence.js
@@ -0,0 +1,20 @@
+// time O(n), space O(1)
+function isValidSubsequence(array, sequence) {
+  let arrIdx = 0;
+  let seqIdx = 0;
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === sequence.length;
+}
+
+// time O(n), space O(1)
+function isValidSubsequence(array, sequence) {
+  let seqIdx = 0;
+  for (const value of array) {
+    if (seqIdx === sequence.length) break;
+    if (sequence[seqIdx] === value) seqIdx++;
+  }
+  return seqIdx === sequence.length;
+}
