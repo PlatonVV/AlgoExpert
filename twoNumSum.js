8e61b7c54f1efd85513a920b39f3893d92c7d0ce
platonVV <platonvolodin@icloud.com>
2023-09-22 10:08:48 -0400

    rep

--- twoNumSum.js
+++ twoNumSum.js
@@ -1,44 +1,30 @@
-// O(n^2) time | O(1) space
-function twoNumberSumFirst (array, targetSum) {
-  for (let i = 0; i < array.length - 1; i++) {
-    const firstNum = array[i];
-    for (let j = i + 1; j < array.length; j++) {
-      const secondNum = array[j];
+// time 0(n^2) space 0(1)
+function twoNumberSum1(array, targetSum) {
+  for (i = 0; i < array.length - 1; i++) {
+    const firstNum = array[i]
+    for (j = i + 1; j < array.length; j++) {
+      const secondNum = array[j]
       if (firstNum + secondNum === targetSum) {
-        return [firstNum, secondNum];
+        return [firstNum, secondNum]
       }
     }
   }
-  return [];
+  return []
 }
 
-// 0(n) time | 0(1) space
-function twoNumberSumSecond(array, targetSum) {
-  const nums = {};
+// time O(n) space O(n)
+function twoNumberSum2(array, targetSum) {
+  const nums = {}
   for (const num of array) {
-    let possibleMatch = targetSum - num;
-    if (possibleMatch in nums) {
-      return [possibleMatch, num];
+    const potentialMatch = targetSum - num
+    if (potentialMatch in nums) {
+      return [potentialMatch, num]
     } else {
-      nums[num] = true;
+      nums[num] = true
     }
   }
-  return [];
+  return []
 }
 
-function twoNumberSumThird(array, targetSum) {
-  array.sort((a, b) => a - b);
-  let left = 0;
-  let right = array.length - 1;
-  while (left < right) {
-    const currentSum = array[left] + array[right];
-    if (currentSum === targetSum) {
-      return [array[left], array[right]];
-    } else if (currentSum < targetSum) {
-      left++;
-    } else if (currentSum > targetSum) {
-      right--;
-    }
-  }
-  return [];
+function twoNumberSum(array, targetSum) {
 }
